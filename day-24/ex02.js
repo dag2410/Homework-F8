f15a3d175188e846755ed435ffd1739596914c2c
Hai Dang <dagger241004abc@gmail.com>
2024-12-23 16:47:49 +0700

    up

--- day-24/ex02.js
+++ day-24/ex02.js
@@ -1,18 +1,23 @@
 let n = parseInt(prompt("Nhập số lượng cho dãy Fibonacci: "));
 function printFibonacci(n) {
-  if (n <= 0 || typeof n == "string") {
-    console.log("Số phần tử không hợp lệ");
-    return;
+  if (
+    n <= 0 ||
+    typeof n == "string" ||
+    isNaN(n) ||
+    !isFinite(n) ||
+    !Number.isInteger(n)
+  ) {
+    return "Số phần tử không hợp lệ";
   }
+
   let result = [0];
   if (n === 1) {
     console.log(result);
-    return;
   }
   result.push(1);
   for (let i = 2; i < n; i++) {
     result.push(result[i - 1] + result[i - 2]);
   }
-  console.log(result);
+  return result.join(" ");
 }
-printFibonacci(n);
+console.log(printFibonacci(n));
